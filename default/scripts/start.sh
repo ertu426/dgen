d86cbf1da12bbf5a5c6cb0a12aac1d6468bb9f46
ertu426 <erxat124@gmail.com>
2026-03-23 18:57:32 +0800

    fix: 优化Docker镜像构建与启动配置

--- default/scripts/start.sh
+++ default/scripts/start.sh
@@ -1,5 +1,21 @@
-#!/bin/bash\n\
-service ssh start\n\
-# --auth none 方便测试，生产环境建议设置密码\n\
-# --bind-addr 0.0.0.0:8080 允许外部访问\n\
-su - dev -c "code-server --bind-addr 0.0.0.0:8080 --auth none
+#!/bin/bash
+
+set_password() {
+    local user="$1"
+    local password="$2"
+
+    encrypted=$(openssl passwd -1 "$password")
+    
+    usermod -p "$encrypted" "$user"
+}
+
+if [ -n "$DEV_PASSWORD" ]; then
+    set_password dev "$DEV_PASSWORD"
+else
+    set_password dev "dev"
+fi
+
+rm -rf /scripts
+service ssh start
+
+exec su -s /bin/bash -c "code-server --bind-addr 0.0.0.0:8080 --auth none /home/dev/workspace" dev
